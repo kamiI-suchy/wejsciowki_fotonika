eaeefa091184db7e806b3d21c41f6372502fabf1
kamiI-suchy <kamilsuchy132@gmail.com>
2026-05-11 10:34:52 +0200

    Add files via upload

---
+++ SERIA 2/02_-_Propagacja_wiazki_gaussowskiej_przez_soczewkę/Gauss_soczewka_skupiajaca.m
@@ -0,0 +1,57 @@
+% Na podstawie znajomoœci parametrów wi¹zki gaussowskiej wejœciowej
+% program oblicza parametry wi¹zki gaussowskiej po przejœciu przez         
+% soczewkê skupiaj¹c¹ o znanej ogniskowej. Korzysta siê z równania           
+% soczewki cienkiej. 
+%*******************************************************************
+clc; clear all; close all;
+i = sqrt(-1);
+%-------------------------------------------------------------------
+%PARAMETRY WI¥ZKI WEJŒCIOWEJ (d³ugoœci w mm)
+%-------------------------------------------------------------------
+lambda = 0.633*10^-3; 	%d³ugoœæ fali lasera He-Ne
+w10 = 0.25; 	%promieñ przewê¿enia wi¹zki
+z10 = (pi/lambda)*w10^2;  	%zasiêg Rayleigha
+theta1 = lambda/(pi*w10); 		%k¹t po³ówkowy rozbie¿noœci wi¹zki
+d1 = 400; 	%odleg³oœæ od przewê¿enia do soczewki
+q1 = d1 - i*z10; 	%parametr Kogelnika
+%-------------------------------------------------------------------
+%SOCZEWKA
+%-------------------------------------------------------------------
+f = 75; 	%ogniskowa soczewki skupiaj¹cej
+S = [1,0; -1/f,1]; 	%macierz dla soczewki   
+A = S(1,1);  B = S(1,2); C = S(2,1); D = S(2,2);
+%-------------------------------------------------------------------
+% RÓWNANIA SOCZEWKI CIENKIEJ DLA WI¥ZKI GAUSSOWSKIEJ
+%-------------------------------------------------------------------
+w20 = sqrt((f^2*w10^2)/((f-d1)^2+z10^2));
+z20 = (pi/lambda)*w20^2;
+d2 = f+f^2*(d1-f)/((f-d1)^2+z10^2);
+theta2=lambda/(pi*w20); %lub theta2=sqrt(lambda/(pi*z20));
+theta_stopnie=theta2*57.325;
+%-------------------------------------------------------------------
+%RÓWNANIE WI¥ZKI GAUSSOWSKIEJ
+%-------------------------------------------------------------------
+z = 0:0.1:d1;
+w1 = w10*sqrt(1+(z).^2/z10^2);
+zz = d1:0.1:2*d1;
+w2 = w20*sqrt(1+(zz-d1-d2).^2/z20^2); 
+%-------------------------------------------------------------------
+%WYKRES przestrzenny wi¹zki
+%-------------------------------------------------------------------
+figure(1);
+plot(z,w1,'k',z,-w1,'k','linewidth',2); hold on;
+plot(z,z*0,'k:',zz,zz*0,'k:');
+plot(zz,w2,'k',zz,-w2,'k','linewidth',2);
+a=1; line=-a:0.1:a; 
+plot(0*line+d1,line,'linewidth',3);
+plot(0*line+d1+f,line,'k:','linewidth',2); hold off;
+ylim([-1 1]); xlabel('[mm]'); ylabel('[\mum]');
+%------------------------------------------------------------------
+%Przekrój wi¹zki w przewê¿eniu przed i za soczewk¹
+%-------------------------------------------------------------------
+alfa=0:0.01:2*pi;
+x1=w10*sin(alfa); y1=w10*cos(alfa);
+x2=w20*sin(alfa); y2=w20*cos(alfa);
+%figure(2);
+%plot(x1,y1,'k',x2,y2,'k','linewidth',2);
+%axis('equal');
